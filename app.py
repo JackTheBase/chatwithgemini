b7abca09ed4e5665bdaacc5c9f513617984f1470
JackTheBase <78374727+JackTheBase@users.noreply.github.com>
2025-04-06 08:10:58 +0000

    to coded gemini_api_key

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import google.generativeai as genai
 
 try:
-    key = st.secrets["AIzaSyCDCBkmMtx5EvK_4-wEk5HvI3fKhNPArsA"]
+    key = st.secrets["gemini_api_key"]
     genai.configure(api_key=key)
     model = genai.GenerativeModel('gemini-2.0-flash-lite')
 
